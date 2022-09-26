9951ff00a0da092766f04a59bdf86a0ad0c07de1
Allison Ause <allison.ause@gmail.com>
2022-09-26 12:22:51 -0700

    fixed the lack of destructuring of id in order to view detail page correctly

--- src/App.js
+++ src/App.js
@@ -4,15 +4,13 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 
 function App() {
-  <Switch>
-    <Route path="/books" component={BookList} />
-    <Route path="/books/:id" component={BookDetail} />
-  </Switch>;
-
   return (
     <main className="container">
       <h1>Library Catalog</h1>
-      <BookList />
+      <Switch>
+        <Route path="/books/:id" component={BookDetail} />
+        <Route path="/books" component={BookList} />
+      </Switch>
     </main>
   );
 }
