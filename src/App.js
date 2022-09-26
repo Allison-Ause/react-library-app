3360a2e287d579dcc0b1cdbc5cdccfe6e085a871
Allison Ause <allison.ause@gmail.com>
2022-09-26 12:09:02 -0700

    defined /books and /books/:id routes. added useParams to BookDetail.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import BookList from './components/book/BookList';
+import BookDetail from './components/book/BookDetail';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
-  // TODO: Add routes to books & views
+  <Switch>
+    <Route path="/books" component={BookList} />
+    <Route path="/books/:id" component={BookDetail} />
+  </Switch>;
+
   return (
     <main className="container">
       <h1>Library Catalog</h1>
