5ed044258e06c355a3d7e37c58cb806def54112c
Allison Ause <allison.ause@gmail.com>
2022-09-26 14:11:43 -0700

    added homepage with link to all books list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import BookList from './components/book/BookList';
 import BookDetail from './components/book/BookDetail';
 import './App.css';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <main className="container">
       <h1>Library Catalog</h1>
+      <Link className="home-link" to="/books">
+        Check Out These Books!
+      </Link>
       <Switch>
         <Route path="/books/:id" component={BookDetail} />
         <Route path="/books" component={BookList} />
